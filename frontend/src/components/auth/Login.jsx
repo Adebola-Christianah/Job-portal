1e7686c479fc14d0ab89ddb41544494270ac6543
Adebola-Christianah <oguntoyeadebola21@gmail.com>
2024-10-11 12:17:16 +0100

    added profile image upload

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -113,7 +113,7 @@ const Login = () => {
                                     onChange={changeEventHandler}
                                     className="cursor-pointer"
                                 />
-                                <Label htmlFor="r1">Student</Label>
+                                <Label htmlFor="r1">Job Seeker</Label>
                             </div>
                             <div className="flex items-center space-x-2">
                                 <Input
